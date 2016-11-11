3e8db802ef597d8be4cb66d53791081d781ceb4b
Pengxm <18614082572@163.cm>
2016-11-11 17:05:09 +0800

    初次提交图片

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ require('styles/App.css');
 
 import React from 'react';
 
-// let imageUrl = require('../images/yeoman.png');
+// let imageUrl = require('../images/1.png');
 
 // 获取图片数据
 var imageDatas = require('../data/imgDatas.json');
@@ -11,21 +11,44 @@ var imageDatas = require('../data/imgDatas.json');
 imageDatas = (function genImageUrl(imageDatasArr) {
    for(var i = 0, j = imageDatasArr.length; i < j; i++){
      var singleImageData = imageDatasArr[i];
-     singleImageData.imageUrl = require('../images/'+ singleImageData.fileName +'.png');
+     singleImageData.imageUrl = require('../images/'+ singleImageData.fileName);
      imageDatasArr[i] = singleImageData;
    }
   return imageDatasArr;
 })(imageDatas);
 
+var ImgFigure = React.createClass({
+  render : function(){
+    return (
+      <figure>
+        <img src={this.props.data.imageUrl} alt={this.props.data.title}/>
+        <figcaption>
+          <h2>
+            {this.props.data.title}
+          </h2>
+        </figcaption>
+      </figure>
+    )
+  }
+});
+
+
+
 class AppComponent extends React.Component {
   render() {
+
+    var controllerUnits = [],
+        imgFigures = [];
+    imageDatas.forEach(function(value){
+      imgFigures.push(<ImgFigure data={value} />)
+    });
     return (
         <section className="stage">
           <section className="image-sec">
-
+            {imgFigures}
           </section>
           <nav className="controller-nav">
-
+            {controllerUnits}
           </nav>
         </section>
     );
